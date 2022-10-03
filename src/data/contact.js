9a6d444b7cc03b70158958dd6bc1d5fe7b091dd8
Tony Davis <acdavis629@gmail.com>
2022-10-03 10:14:48 -0400

    add medium icon

--- src/data/contact.js
+++ src/data/contact.js
@@ -4,6 +4,7 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
 // import { faAngellist } from '@fortawesome/free-brands-svg-icons/faAngellist';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
+import { faMedium} from '@fortawesome/free-brands-svg-icons/faMedium';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
 // to add other icons.
@@ -38,6 +39,11 @@ const data = [
     link: 'https://twitter.com/TonyD993',
     label: 'Twitter',
     icon: faTwitter,
+  },
+  {
+    link: 'https://medium.com/@tdavis629',
+    label: 'Medium',
+    icon: faMedium,
   },
   {
     link: 'mailto:acdavis629@gmail.com',
