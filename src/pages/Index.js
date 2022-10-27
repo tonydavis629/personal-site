a5cb0d4ea4b7949cd45b9387755295bdd4b9d96d
Tony Davis <acdavis629@gmail.com>
2022-10-27 12:12:53 -0400

    index

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,20 +5,19 @@ import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description={"Michael D'Angelo's personal website. New York based Stanford ICME graduate, "
-    + 'co-founder and CTO of Arthena, and YC Alumni.'}
+    description={"Tony Davis's personal website. Mechanical engineer, AI researcher, and "
+    + 'life enthusiast.'}
   >
     <article className="post" id="index">
       <header>
         <div className="title">
-          <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
+          <h2 data-testid="heading"><Link to="/">Welcome to my website!</Link></h2>
           <p>
-            A beautiful, responsive, statically-generated,
-            react application written with modern Javascript.
+            Browse around to get to know me, my interests, and my work.
           </p>
         </div>
       </header>
-      <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
+      <p> You can find personal information in <Link to="/about">about me</Link>,
         or you can check out my {' '}
         <Link to="/resume">resume</Link>, {' '}
         <Link to="/projects">projects</Link>, {' '}
