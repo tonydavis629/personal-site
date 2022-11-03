2b5280a7a0a41625eb4e2407125b7b3ce8839da6
Tony Davis <acdavis629@gmail.com>
2022-11-03 16:41:07 -0400

    add haucs

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,15 +1,14 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: 'Nearest Dollar',
-    subtitle: '2015 BVP Hackathon',
-    image: '/images/projects/nearestdollar.jpg',
-    date: '2015-11-20',
+    title: 'The Hybrid Aerial Underwater Robotic System',
+    subtitle: 'Master\'s Thesis',
+    link: 'https://github.com/tonydavis629/HAUCS',
+    image: '/images/projects/haucs2.jpg',
+    date: '2022-11-20',
     desc:
-      'Built for a social impact hackathon. '
-      + 'NearestDollar connected to your bank accounts, credit cards, '
-      + 'or debit cards and rounded up your purchases to donate the balance to '
-      + 'the charity of your choice.',
+      'My Master\'s thesis to develop a path planning algorithm for a robotic water quality monitoring system.'
+      + 'It is intended for aquaculture farmers to easily maintain appropriate water quality for their fish.'
   },
   {
     title: 'Harvest',
