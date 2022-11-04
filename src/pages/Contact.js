5dc6856c5aca04d743a47122502d911e1916fdcf
Tony Davis <acdavis629@gmail.com>
2022-11-04 17:27:47 -0400

    fix contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -17,8 +17,8 @@ const Contact = () => (
         </div>
       </header>
       <div className="email-at">
-        <p>Feel free to get in touch. You can email me at: </p>
-        <p><a href="mailto:acdavis629@gmail.com">acdavis629@gmail.com</a></p>
+        <p>Feel free to get in touch. You can email me at: 
+        <a href="mailto:acdavis629@gmail.com">acdavis629@gmail.com</a></p>
       </div>
       <ContactIcons />
     </article>
