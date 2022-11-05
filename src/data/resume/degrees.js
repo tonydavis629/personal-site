16088a3059e640bf1b500f0c6dd3257df52dc6ca
Tony Davis <acdavis629@gmail.com>
2022-11-05 16:18:17 -0400

    degree and positions

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,15 +1,15 @@
 const degrees = [
   {
-    school: 'Stanford University',
-    degree: 'M.S. Computational and Mathematical Engineering',
-    link: 'https://stanford.edu',
-    year: 2016,
+    school: 'Florida Atlantic University',
+    degree: 'M.S. Artificial Intelligence',
+    link: 'https://www.fau.edu/',
+    year: 2022,
   },
   {
-    school: 'University at Buffalo',
-    degree: 'B.S. Electrical Engineering, Computer Engineering',
-    link: 'https://buffalo.edu',
-    year: 2012,
+    school: 'Ohio State University',
+    degree: 'B.S. Mechanical Engineering',
+    link: 'https://www.osu.edu/',
+    year: 2016,
   },
 ];
 
