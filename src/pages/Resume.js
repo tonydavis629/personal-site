5c401b7ae59c79a58e57993e063e2399ae19c51a
Tony Davis <acdavis629@gmail.com>
2022-11-04 22:52:18 -0400

    fix skills remove

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -5,19 +5,18 @@ import Main from '../layouts/Main';
 
 import Education from '../components/Resume/Education';
 import Experience from '../components/Resume/Experience';
-import Skills from '../components/Resume/Skills';
+// import Skills from '../components/Resume/Skills';
 import Courses from '../components/Resume/Courses';
 import References from '../components/Resume/References';
 
 import courses from '../data/resume/courses';
 import degrees from '../data/resume/degrees';
 import positions from '../data/resume/positions';
-import { skills, categories } from '../data/resume/skills';
+// import { skills, categories } from '../data/resume/skills';
 
 const sections = [
   'Education',
   'Experience',
-  'Skills',
   'Courses',
   'References',
 ];
