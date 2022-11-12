d519f33c53eff10743201bce8a08f32f5c2dec24
Tony Davis <acdavis629@gmail.com>
2022-11-11 20:48:21 -0500

    bubblegum

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -25,6 +25,7 @@ const Index = () => (
         view <Link to="/stats">site statistics</Link>, {' '}
         or <Link to="/contact">contact</Link> me.
       </p>
+      <p>Some people can read War and Peace and come away thinking it's a simple adventure story. Others can read the ingredients on a chewing gum wrapper and unlock the secrets of the universe.</p>
       <p> Source available <a href="https://github.com/tonydavis629/personal-site">here</a>.</p>
     </article>
   </Main>
