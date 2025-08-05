2b3b57b059c7f0aed09f1f097c8625b6f0f927a9
Tony Davis <acdavis629@gmail.com>
2025-08-04 20:47:58 -0400

    about

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -19,7 +19,7 @@ const SideBar = () => (
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Tony. I&apos;m an engineer, adventurer, and life enthusiast.
+      <p>I'm Tony. Leader, lover, learner. 
       </p>
       <ul className="actions">
         <li>
